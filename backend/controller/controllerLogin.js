36a7a3d2258afa297a4dfdcc042ea98edf53ad52
Dana <dana@hlavacova.de>
2023-09-09 21:26:14 +0200

    profil work withoud user.role

--- backend/controller/controllerLogin.js
+++ backend/controller/controllerLogin.js
@@ -1,4 +1,4 @@
-import { User } from "../model/modelSchema.js";
+import {User} from "../model/modelSchema.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
@@ -11,39 +11,43 @@ dotenv.config();
 
 //login User
 export const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res
-        .status(404)
-        .send({ success: false, error: "User/Password Combination not found" });
-    }
-    const isPasswordValid = await user.isPasswordValid(password);
-    if (!isPasswordValid) {
-      return res
-        .status(404)
-        .send({ success: false, error: "Password not found" });
-    }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if (!user) {
+            return res
+                .status(404)
+                .send({success: false, error: "User/Password Combination not found"});
+        }
+        const isPasswordValid = await user.isPasswordValid(password);
+        if (!isPasswordValid) {
+            return res
+                .status(404)
+                .send({success: false, error: "Password not found"});
+        }
+        const token = jwt.sign({id: user._id, "role": user.role}, process.env.JWT_SECRET, {
+            expiresIn: "24h",
+        });
 
-    res
-      .cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-      })
-      /*   .json({
-              id: user._id,
-              name: user.name,
-              email: user.email,
-              role: user.role,
-            })*/
-      .status(200)
-      .send({ success: true, token, msg: `User logged in` });
-  } catch (error) {
-    res.status(500).send({ success: false, error: error.message });
-    console.error(error.message);
-  }
+        res
+            .cookie("token", token, {
+                httpOnly: true,
+                secure: true,
+            })
+
+            .status(200)
+            .json({
+                success: true,
+                token,
+                msg: `User logged in`,
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            })
+console.log(res.data)
+    } catch (error) {
+        res.status(500).send({success: false, error: error.message});
+        console.error(error.message);
+    }
 };
